9f7a3f098e38667ceedf74394a24890d9d0eaf43
LauraDa999 <141860731+LauraDa999@users.noreply.github.com>
2023-10-10 21:32:52 -0500

    Add files via upload

---
+++ 2punto.py
@@ -0,0 +1,9 @@
+
+print("Números impares desde 1 hasta 999:")
+for numero_impar in range(1, 1000, 2):
+    print(numero_impar)
+
+
+print("\nNúmeros pares desde 2 hasta 1000:")
+for numero_par in range(2, 1001, 2):
+    print(numero_par)
